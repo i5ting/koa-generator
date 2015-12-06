f805a13081a5b629eb3f35903824f2f26c9cda97
i5ting <i5ting@126.com>
2015-12-06 10:50:37 +0800

    init

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ http://koajs.com/
 - https://github.com/queckezz/koa-views
 - https://github.com/koajs/static
 - https://github.com/koajs/logger
+- https://github.com/koajs/json
+- https://github.com/koajs/body-parsers
+- cookies已经koa内置了，无需处理
+
+
+- https://github.com/koajs/mount 很好，然没用到
+- https://github.com/zedgu/surface rest api可参考
--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var app = require('koa')();
 var koa = require('koa-router')();
 var logger = require('koa-logger');
-
+var json = require('koa-json');
+var views = require('koa-views');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-
+// global middlewares
+app.use(views('views', {
+  root: __dirname + '/views',
+  default: 'jade'
+}));
+app.use(require('koa-bodyparser')());
+app.use(json());
 app.use(logger());
 
 app.use(function *(next){
@@ -18,16 +25,15 @@ app.use(function *(next){
 
 app.use(require('koa-static')(__dirname + '/public'));
 
+// routes definition
 koa.use('/', index.routes(), index.allowedMethods());
 koa.use('/users', users.routes(), users.allowedMethods());
 
+// mount root routes  
 app.use(koa.routes());
 
-
-
+app.on('error', function(err, ctx){
+  log.error('server error', err, ctx);
+});
 
 module.exports = app;
-
-// app.listen(3000);
-
-// console.log('listening on port 3000');
